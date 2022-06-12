e71aaecc2bb54894f2101505de03a9d897670d4e
zjavax <1306187161@qq.com>
2022-06-12 21:04:56 +0800

    1

--- src/main/java/com/bloxbean/cardano/client/examples/Constant.java
+++ src/main/java/com/bloxbean/cardano/client/examples/Constant.java
@@ -1,5 +1,11 @@
 package com.bloxbean.cardano.client.examples;
 
 public class Constant {
-    public static final String BF_PROJECT_KEY = "<project_id>";
+//    public static final String BF_PROJECT_KEY = "<project_id>";
+
+    public static final String BF_PROJECT_KEY = "testnetxf0Kv0lwHjAbQxLmd55h5vDcLmkFXOQh";
+
+
+    // 谷歌账号
+    public static final String BF_PROJECT_KEY_MAINNET = "mainnetgwvbF0pMwaC0hXbgy0dxumkLEfTebsbq";
 }
