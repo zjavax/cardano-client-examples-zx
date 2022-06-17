3c2eb1c7ebb136096724bf3c14848b83b9c1f4d6
zjavax <1306187161@qq.com>
2022-06-17 21:12:24 +0800

    优化代码

--- src/main/java/cn/zjavax/BuildWithJavaBackend3_asset.java
+++ src/main/java/cn/zjavax/BuildWithJavaBackend3_asset.java
@@ -13,15 +13,13 @@ import com.bloxbean.cardano.client.function.TxBuilderContext;
 import com.bloxbean.cardano.client.function.TxOutputBuilder;
 import com.bloxbean.cardano.client.transaction.spec.Transaction;
 import com.bloxbean.cardano.client.util.HexUtil;
+import com.bloxbean.cardano.client.util.JsonUtil;
 import com.fasterxml.jackson.databind.JsonNode;
 
 import java.math.BigDecimal;
-import java.math.BigInteger;
 import java.nio.charset.StandardCharsets;
 import java.util.List;
 
-import static com.bloxbean.cardano.client.common.ADAConversionUtil.adaToLovelace;
-import static com.bloxbean.cardano.client.common.CardanoConstants.LOVELACE;
 import static com.bloxbean.cardano.client.function.helper.ChangeOutputAdjustments.adjustChangeOutput;
 import static com.bloxbean.cardano.client.function.helper.FeeCalculators.feeCalculator;
 import static com.bloxbean.cardano.client.function.helper.InputBuilders.createFromSender;
@@ -32,25 +30,29 @@ public class BuildWithJavaBackend3_asset extends BaseTest {
 //        String senderMnemonic = "kit color frog trick speak employ suit sort bomb goddess jewel primary spoil fade person useless measure manage warfare reduce few scrub beyond era";
         String senderAddress = "addr_test1qqxnp3khzm7kcj9t23hskehat7428ghsenk0pfew4rqy5v9frnmht7uwrl073q4jvq20z82kh4rksyns540azhndqexqpvhgqr";
 
-        TransferAsset transferAsset = new TransferAsset();
-        transferAsset.setPolicyId("769c4c6e9bc3ba5406b9b89fb7beb6819e638ff2e2de63f008d5bcff");
-        transferAsset.setAssetName("tNEWM");
-        transferAsset.setAmount(10.000001);
-        transferAsset.setReceiverAddress("addr_test1qq4pxvsevncnfd7ppmatyavej7kjcukwxugrnk9rsejufvpfqqgflwz7ahhqezd8mx5hfxmwh2stfagm8uwkxreya6rsqfxnz7");
+//        TransferAsset transferAsset = new TransferAsset();
+//        transferAsset.setPolicyId("769c4c6e9bc3ba5406b9b89fb7beb6819e638ff2e2de63f008d5bcff");
+//        transferAsset.setAssetName("tNEWM");
+//        transferAsset.setAmount(10.000001);
+//        transferAsset.setReceiverAddress("addr_test1qq4pxvsevncnfd7ppmatyavej7kjcukwxugrnk9rsejufvpfqqgflwz7ahhqezd8mx5hfxmwh2stfagm8uwkxreya6rsqfxnz7");
 
         TransferAsset transferAsset1 = new TransferAsset();
         transferAsset1.setPolicyId("75d01e750b6e4986f4d26f92d70c70a9d337e834faebcc177a7e796a");
         transferAsset1.setAssetName("TestCoin");
-        transferAsset1.setAmount(10);
+        transferAsset1.setAmount(1);
+        transferAsset1.setDecimals(0l);
         transferAsset1.setReceiverAddress("addr_test1qq4pxvsevncnfd7ppmatyavej7kjcukwxugrnk9rsejufvpfqqgflwz7ahhqezd8mx5hfxmwh2stfagm8uwkxreya6rsqfxnz7");
         TransferAsset transferAsset2 = new TransferAsset();
         transferAsset2.setPolicyId("75d01e750b6e4986f4d26f92d70c70a9d337e834faebcc177a7e796a");
         transferAsset2.setAssetName("TestCoin");
-        transferAsset2.setAmount(10);
+        transferAsset2.setAmount(1);
+        transferAsset2.setDecimals(0l);
         transferAsset2.setReceiverAddress("addr_test1qqqvjp4ffcdqg3fmx0k8rwamnn06wp8e575zcv8d0m3tjn2mmexsnkxp7az774522ce4h3qs4tjp9rxjjm46qf339d9sk33rqn");
 
-        List<TransferAsset> transferAssetList = List.of(transferAsset, transferAsset1,transferAsset2);
+        List<TransferAsset> transferAssetList = List.of(transferAsset1,transferAsset2);
 
+        String prettyJson = JsonUtil.getPrettyJson(transferAssetList);
+        System.out.println(prettyJson);
         new BuildWithJavaBackend3_asset().transfer(senderAddress, transferAssetList);
         System.exit(1);
     }
@@ -88,7 +90,7 @@ public class BuildWithJavaBackend3_asset extends BaseTest {
         String assetNameHexString = HexUtil.encodeHexString(assetName.getBytes(StandardCharsets.UTF_8));
         Result<Asset> asset = assetService.getAsset(policyId + assetNameHexString);
         JsonNode decimals = asset.getValue().getMetadata().get("decimals");
-        return decimals.asLong();
+        return decimals == null ? 0 : decimals.asLong();
     }
 }
 
